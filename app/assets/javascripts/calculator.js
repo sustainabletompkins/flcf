836c8a371e9a0f91e0228c4dc334a15a7f150b83
Swidjit <alex@swidjit.com>
2015-01-14 13:44:15 -0500

    displaying cart price as float

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -3,11 +3,11 @@ function totalCart() {
 	total_cost = 0;
 	$('.cost').each(function() {
 		console.log($(this).text());
-		total_cost += parseInt($(this).text());
+		total_cost += parseFloat($(this).text());
 
 	});
 	console.log(total_cost);
-	$('#total-cost .value').html("$"+total_cost);
+	$('#total-cost .value').html("$"+total_cost.toFixed(1));
 
 }
 
