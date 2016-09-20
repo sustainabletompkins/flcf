a4815770bcdc210ff0129196742c13f719cf1fab
Alex C <alex@swidjit.com>
2016-09-20 17:43:39 -0400

    fixing background loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,15 +30,16 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
