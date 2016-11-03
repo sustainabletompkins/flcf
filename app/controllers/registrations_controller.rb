7abf0c74bab43f3ffe561b1b60e91713cfece6c1
Alex C <alex@swidjit.com>
2016-11-03 18:55:48 -0400

    properly add offset count when adding to carbon races

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,7 @@ class RegistrationsController < Devise::RegistrationsController
         o.save
         pounds = pounds = o.pounds
       end
-      Individual.create(:name=>resource.name, :pounds=>pounds, :count=>1, :email => resource.email)
+      Individual.create(:name=>resource.name, :pounds=>pounds, :count=>@user_offsets.count, :email => resource.email)
       cookies[:joined] = "true"
 
     end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,7 @@ class TeamsController < ApplicationController
   def create
     @league = Team.create(team_params)
     if team_params.has_key?(:pounds)
-      @league.update_attribute(:count, 1)
+      @league.update_attribute(:count, params[:count].to_i)
       @league.update_attribute(:members, 1)
       @league.increment!(:pounds, params[:pounds].to_i)
       render 'team_created_after_offset'
@@ -11,16 +11,16 @@ class TeamsController < ApplicationController
       @count = Team.all.count
       render 'team_created'
     end
-    TeamMember.create(:email => params[:user_email], :name=> params[:member_name], :offsets => 1, :founder=> "TRUE", :team_id=>@league.id)
+    TeamMember.create(:email => params[:user_email], :name=> params[:member_name], :offsets => params[:count].to_i, :founder=> "TRUE", :team_id=>@league.id)
   end
 
   def join
     @team = Team.find(params[:league_id])
     @pounds = params[:pounds]
-    @team.increment!(:count, 1)
+    @team.increment!(:count, params[:count].to_i)
     @team.increment!(:members, 1)
     @team.increment!(:pounds, params[:pounds].to_i)
-    TeamMember.create(:email => params[:user_email], :offsets => 1, :team_id=>@team.id, :name=> params[:name])
+    TeamMember.create(:email => params[:user_email], :offsets => params[:count].to_i, :team_id=>@team.id, :name=> params[:name])
     render 'team_joined_after_offset'
 
   end
