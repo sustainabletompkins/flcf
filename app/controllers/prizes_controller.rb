efe26fff4ed8159f6b83ce8a90778f01752c04bd
Alex C <alex@swidjit.com>
2016-11-22 04:49:13 -0500

    fixing prize wheel

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -1,12 +1,9 @@
 class PrizesController < ApplicationController
 
   def save
-    rec = PrizeWinner.where(:prize_id=>params[:prize], :email=>nil).first
-    rec.update_attribute(:email, params[:email])
+    rec = PrizeWinner.create(:prize_id=>params[:prize], :email=>params[:email], :name=> params[:name])
     prize = rec.prize
     prize.decrement!(:count)
-
-    rec.update_attribute(:name, params[:name]) if params[:name].length > 0
     #PrizeMailer.send_prize_details(params[:email],rec.code,rec.prize.title,"this is a test")
   end
 
