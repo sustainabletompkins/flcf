359c7f3d0af41b944afae394b0e052bb3eec4d18
Alex C <alex@swidjit.com>
2017-01-25 10:24:32 -0500

    admin page and offset log

--- app/controllers/offsets_controller.rb
+++ app/controllers/offsets_controller.rb
@@ -25,7 +25,7 @@ class OffsetsController < ApplicationController
   def manual_create
     if simple_captcha_valid?
       pounds = params[:offset][:cost].to_i * 80
-      @offset = Offset.create(:purchased => 'TRUE',:user_id=>'0',:title=>params[:offset][:title],:cost=>params[:offset][:cost],:pounds=>pounds,:email => params[:user_email],:zipcode=>params[:offset][:zipcode])
+      @offset = Offset.create(:purchased => 'TRUE',:user_id=>'0',:name=>params[:offset][:name], :title=>params[:offset][:title],:cost=>params[:offset][:cost],:pounds=>pounds,:email => params[:user_email],:zipcode=>params[:offset][:zipcode])
       @stat = Stat.all.first
       @stat.increment!(:pounds, pounds)
       @stat.increment!(:dollars, params[:offset][:cost].to_f)
