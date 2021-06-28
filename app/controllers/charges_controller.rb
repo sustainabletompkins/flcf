ffe004fa1fa354ef29d68f14081924a74773c24a
Sustainable Tompkins <info@sustainabletompkins.org>
2021-06-28 15:47:25 -0500

    removing redundant column in db migration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,7 +16,7 @@ class ChargesController < ApplicationController
       payment_method_types: ['card'],
       line_items: products,
       mode: payment_mode,
-      success_url: 'http://localhost:3000/charges/success?session_id={CHECKOUT_SESSION_ID}',
+      success_url: 'http://flcf-staging.herokuapp.com/charges/success?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'http://localhost:3000/charges/failure',
     })
   end
@@ -50,7 +50,7 @@ class ChargesController < ApplicationController
       payment_method_types: ['card'],
       line_items: products,
       mode: payment_mode,
-      success_url: 'http://localhost:3000/payment-success?checkout_session_id={CHECKOUT_SESSION_ID}',
+      success_url: 'http://flcf-staging.herokuapp.com/payment-success?checkout_session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'http://localhost:3000/'
     })
     
--- db/migrate/20210316130721_add_recurring_options_to_cart_items.rb
+++ db/migrate/20210316130721_add_recurring_options_to_cart_items.rb
@@ -1,7 +1,5 @@
 class AddRecurringOptionsToCartItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :cart_items, :offset_type, :string
     add_column :cart_items, :frequency, :string
-    add_column :cart_items, :offset_interval, :string
   end
 end
