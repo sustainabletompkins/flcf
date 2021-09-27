2567b220486fc815f6b13997baafa17584b20578
Sustainable Tompkins <info@sustainabletompkins.org>
2021-09-27 10:40:53 -0400

    only add to leaderboard if offsets found

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,6 +9,9 @@ class Team < ActiveRecord::Base
 
   def self.generate_leaderboard(start_date = nil, end_date = nil, region = nil, limit = 0, offset = 0, solo_mode = nil)
     results = []
+    puts region
+    region = Region.where(name: region).first
+    puts region.inspect
     start_date = if start_date.nil?
                    Time.now - 20.years
                  else
@@ -23,7 +26,7 @@ class Team < ActiveRecord::Base
     if solo_mode
       Individual.all.each do |team|
         offsets = team.offsets.where('created_at > ? and created_at < ?', start_date, end_date)
-        offsets = offsets.where(region: Region.where(name: region).first) if region.present?
+        offsets = offsets.where(region: region) if region.present?
         results << { team: team.name, pounds: offsets.sum(&:pounds), count: offsets.count }
       end
     else
@@ -33,8 +36,11 @@ class Team < ActiveRecord::Base
                   else
                     team.offsets
                   end
-        offsets = offsets.where(region: Region.where(name: region).first) if region.present?
-        results << { team: team.name, pounds: offsets.sum(&:pounds), count: offsets.count, region: team.region.name, image: team.image.attached? ? Rails.application.routes.url_helpers.url_for(team.image) : '' }
+        offsets = offsets.where(region: region) if region.present?
+        if offsets.count > 0
+          results << { team: team.name, pounds: offsets.sum(&:pounds), count: offsets.count, region: team.region.name, image: team.image.attached? ? Rails.application.routes.url_helpers.url_for(team.image) : '' }
+
+        end
       end
     end
     results = results.sort_by { |k| k[:pounds] }.reverse
