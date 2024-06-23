9be143fb2be934c314db8ef99df7a8b670999214
Alex Colket <Swidjit>
2024-06-23 13:03:58 -0400

    trying to make individual users work better

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,35 +1,49 @@
 class IndividualsController < ApplicationController
-
-
   def create
-    offsets = Offset.where(:checkout_session_id => params[:checkout_session_id])
-    @i = Individual.where(:email=>offsets.first.email).first
-    if @i.present?
-      
-    else
+    offsets = Offset.where(checkout_session_id: params[:checkout_session_id])
+    @individual = Individual.find_or_initialize_by(email: offsets.first.email)
+    
+    if @individual.new_record?
       region = Region.get_by_zip(offsets.first.zipcode)
-      @i=Individual.create(:name=>params[:individual][:name], :email => offsets.first.email, :region=>region)
-    end
-    offsets.update_all(:individual_id => @i.id)
-    # redirect to standings
-  end
-  
-  def update
-    @user = Individual.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(individual_params)
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.json { respond_with_bip(@user) }
+      @individual.assign_attributes(name: params[:individual][:name], region: region)
+      if @individual.save
+        offsets.update_all(individual_id: @individual.id)
+        respond_to do |format|
+          format.html { redirect_to @individual, notice: 'Individual was successfully created.' }
+          format.json { render json: @individual, status: :created }
+        end
       else
-        format.html { render :action => "edit" }
-        format.json { respond_with_bip(@user) }
+        respond_to do |format|
+          format.html { render :new }
+          format.json { render json: @individual.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      offsets.update_all(individual_id: @individual.id)
+      respond_to do |format|
+        format.html { redirect_to @individual, notice: 'Individual was successfully updated.' }
+        format.json { render json: @individual, status: :ok }
       end
     end
   end
+
+  def update
+    @individual = Individual.find(params[:id])
+
+    respond_to do |format|
+      if @individual.update(individual_params)
+        format.html { redirect_to @individual, notice: 'Individual was successfully updated.' }
+        format.json { render json: @individual, status: :ok }
+      else
+        format.html { render :edit }
+        format.json { render json: @individual.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
   def individual_params
     params.require(:individual).permit(:name, :pounds)
   end
-
 end
